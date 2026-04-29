972716e9b6d19a7173cb879c414dbec05873169d
ChuUpa <rusnakkaleksandr@gmail.com>
2026-04-29 23:39:56 +0300

    create main, go.mod and .gitignore

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ChuUpa/door-union-backend
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Дверной Союз "
+	fmt.Printf("Привет от %s, это бекэнд этого сайта!\n", name)
+}
