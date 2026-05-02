35016098e50a82b857f85aff93c9aa0651e43b7d
ChuUpa <rusnakkaleksandr@gmail.com>
2026-05-03 00:10:08 +0300

    chore: add chi router with root, health and info endpoints

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ChuUpa/door-union-backend
 
 go 1.26.2
+
+require github.com/go-chi/chi/v5 v5.2.5
--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
-	name := "Дверной Союз "
-	fmt.Printf("Привет от %s, это бекэнд этого сайта!\n", name)
+	r := chi.NewRouter()
+
+	r.Get("/", rootHandler)
+	r.Get("/api/v1/health", healthHandler)
+	r.Get("/api/v1/info", infoHandler)
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		fmt.Printf("Сервер не запустился: %v\n", err)
+	}
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Дверной союз")
+	fmt.Fprintln(w, "Бэкенд сервиса монтажа дверей в Москве")
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "ОК")
+
+}
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Установка и монтаж дверей, Москва и МО, 2010г.")
+
 }
